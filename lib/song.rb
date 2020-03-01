4205e58a442be8b404d4b026969df0a8db86cfbe
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-01 19:39:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(title)
-    self.all.each do {|song_name| song_name.name == title}
+    result = self.all.each do {|song_name| song_name.name == title}
     #self.all.include? (title)
   end
 
