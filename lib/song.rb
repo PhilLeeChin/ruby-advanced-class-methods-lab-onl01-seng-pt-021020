70419c0652dbd99ece0454241e77acb66bd5fbe5
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-01 17:28:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,6 @@ class Song
   attr_accessor :name, :artist
   @@all = []
 
-  def initialize(song_name, artist_name)
-    @name = song_name
-    @artist = artist_name
-  end
-
   def self.create
     @@all << self
   end
